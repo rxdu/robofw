46b2b0677968a1c1eeb4b1d6404c46a5f1860eb5
Ruixiang Du <ruixiang.du@gmail.com>
2022-01-16 22:32:37 +0800

    led blinking works

--- boards/arm/tbot_ctrl_f405rg/board.cmake
+++ boards/arm/tbot_ctrl_f405rg/board.cmake
@@ -2,7 +2,7 @@
 
 board_runner_args(jlink "--device=STM32F405RG" "--speed=4000")
 
-# include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)     ##comment when using jlink
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)     ##comment when using jlink
+# include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 set(BOARD_CPU "cm4")
--- src/app/blinky/main.c
+++ src/app/blinky/main.c
@@ -13,16 +13,42 @@
 #define SLEEP_TIME_MS 500
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0 DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS DT_GPIO_FLAGS(LED0_NODE, gpios)
+#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
+#define LED1 DT_GPIO_LABEL(LED1_NODE, gpios)
+#define LED1_PIN DT_GPIO_PIN(LED1_NODE, gpios)
+#define LED1_FLAGS DT_GPIO_FLAGS(LED1_NODE, gpios)
 #else
 /* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0 ""
+#error "Unsupported board: led1 devicetree alias is not defined"
+#define LED1 ""
+#define PIN 0
+#define FLAGS 0
+#endif
+
+#if DT_NODE_HAS_STATUS(LED2_NODE, okay)
+#define LED2 DT_GPIO_LABEL(LED2_NODE, gpios)
+#define LED2_PIN DT_GPIO_PIN(LED2_NODE, gpios)
+#define LED2_FLAGS DT_GPIO_FLAGS(LED2_NODE, gpios)
+#else
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led2 devicetree alias is not defined"
+#define LED2 ""
+#define PIN 0
+#define FLAGS 0
+#endif
+
+#if DT_NODE_HAS_STATUS(LED3_NODE, okay)
+#define LED3 DT_GPIO_LABEL(LED3_NODE, gpios)
+#define LED3_PIN DT_GPIO_PIN(LED3_NODE, gpios)
+#define LED3_FLAGS DT_GPIO_FLAGS(LED3_NODE, gpios)
+#else
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led3 devicetree alias is not defined"
+#define LED3 ""
 #define PIN 0
 #define FLAGS 0
 #endif
@@ -30,22 +56,30 @@
 void main(void) {
   printk("Starting board: %s\n", CONFIG_BOARD);
 
-  const struct device *dev;
   bool led_is_on = true;
   int ret;
 
-  dev = device_get_binding(LED0);
-  if (dev == NULL) {
+  const struct device *dev_led1 = device_get_binding(LED1);
+  const struct device *dev_led2 = device_get_binding(LED2);
+  const struct device *dev_led3 = device_get_binding(LED3);
+
+  if (dev_led1 == NULL || dev_led2 == NULL || dev_led3 == NULL) {
     return;
   }
 
-  ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+  ret = gpio_pin_configure(dev_led1, LED1_PIN, GPIO_OUTPUT_ACTIVE | LED1_FLAGS);
+  ret = gpio_pin_configure(dev_led2, LED2_PIN, GPIO_OUTPUT_ACTIVE | LED2_FLAGS);
+  ret = gpio_pin_configure(dev_led3, LED3_PIN, GPIO_OUTPUT_ACTIVE | LED3_FLAGS);
+
   if (ret < 0) {
     return;
   }
 
   while (1) {
-    gpio_pin_set(dev, PIN, (int)led_is_on);
+    gpio_pin_set(dev_led1, LED1_PIN, (int)led_is_on);
+    gpio_pin_set(dev_led2, LED2_PIN, (int)led_is_on);
+    gpio_pin_set(dev_led3, LED3_PIN, (int)led_is_on);
+
     led_is_on = !led_is_on;
     k_msleep(SLEEP_TIME_MS);
   }
