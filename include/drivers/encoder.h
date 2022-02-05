6bc8a1624684ee3c7a5121031aa0ab600deb6bf4
Ruixiang Du <ruixiang.du@gmail.com>
2022-02-05 22:11:37 +0800

    encoder: basic encoder interface of stm32 works

--- include/drivers/encoder.h
+++ include/drivers/encoder.h
@@ -24,7 +24,7 @@
 #include <stddef.h>
 #include <sys/math_extras.h>
 #include <device.h>
-// #include <dt-bindings/encoder/encoder.h>
+#include <dt-bindings/encoder/encoder.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,28 +36,47 @@ extern "C" {
 typedef uint8_t encoder_flags_t;
 
 /**
- * @typedef encoder_pin_set_t
- * @brief Callback API upon setting the pin
- * See @a encoder_pin_set_cycles() for argument description
- */
-typedef int (*encoder_pin_set_t)(const struct device *dev, uint32_t encoder,
-			     uint32_t period_cycles, uint32_t pulse_cycles,
-			     encoder_flags_t flags);
-
-/**
  * @typedef encoder_get_counts_t
- * @brief Callback API upon getting cycles per second
+ * @brief Get the counter value
  * See @a encoder_get_cycles_per_sec() for argument description
  */
-typedef int (*encoder_get_counts_t)(const struct device *dev,
-					uint32_t encoder,
-					uint64_t *cycles);
+typedef int (*encoder_get_counts_t)(const struct device *dev, uint32_t *counts);
+
+/**
+ * @typedef encoder_is_counting_up_t
+ * @brief Callback API upon getting counts
+ * See @a encoder_get_cycles_per_sec() for argument description
+ */
+typedef int (*encoder_is_counting_up_t)(const struct device *dev,
+                                        bool *counting_up);
 
 /** @brief ENCODER driver API definition. */
-__subsystem struct encoder_driver_api {
-	encoder_pin_set_t pin_set;
-	encoder_get_counts_t get_counts;
+struct encoder_driver_api {
+  encoder_get_counts_t get_counts;
+  encoder_is_counting_up_t is_counting_up;
 };
+
+static inline int encoder_get_counts(const struct device *dev,
+                                     uint32_t *counts) {
+  const struct encoder_driver_api *api = (struct encoder_driver_api *)dev->api;
+
+  if (api->get_counts == NULL) {
+    return -ENOSYS;
+  }
+
+  return api->get_counts(dev, counts);
+}
+
+static inline int encoder_is_counting_up(const struct device *dev,
+                                         bool *counting_up) {
+  const struct encoder_driver_api *api = (struct encoder_driver_api *)dev->api;
+
+  if (api->is_counting_up == NULL) {
+    return -ENOSYS;
+  }
+
+  return api->is_counting_up(dev, counting_up);
+}
 
 #ifdef __cplusplus
 }
--- src/app/tbot/main.c
+++ src/app/tbot/main.c
@@ -14,8 +14,39 @@
 
 #define SLEEP_TIME_MS 50
 
+#include "drivers/encoder.h"
+
 void main(void) {
   printk("Starting board: %s\n", CONFIG_BOARD);
+
+  const struct device *dev1 = device_get_binding("TIM1_ENCODER");
+  if (dev1 == NULL) {
+    printk("Failed to find encoder device 1\n");
+  } else {
+    printk("[INFO] Initialized encoder device 1\n");
+  }
+
+  const struct device *dev2 = device_get_binding("TIM3_ENCODER");
+  if (dev2 == NULL) {
+    printk("Failed to find encoder device 3\n");
+  } else {
+    printk("[INFO] Initialized encoder device 3\n");
+  }
+
+  uint32_t count1 = 0;
+  bool is_up1 = false;
+  uint32_t count2 = 0;
+  bool is_up2 = false;
+  while (1) {
+    encoder_get_counts(dev1, &count1);
+    encoder_is_counting_up(dev1, &is_up1);
+    encoder_get_counts(dev2, &count2);
+    encoder_is_counting_up(dev2, &is_up2);
+
+    printk("1: count %s: %d, 2: count %s: %d\n", is_up1 ? "up" : "down", count1,
+           is_up2 ? "up" : "down", count2);
+    k_msleep(500);
+  }
 
   if (!InitRobot()) {
     printk("[ERROR] Failed to initialize robot\n");
