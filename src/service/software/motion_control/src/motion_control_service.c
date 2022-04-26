fb01cf9ecfb13729fdc4643e916e2bb96e40c9fe
Ruixiang Du <ruixiang.du@gmail.com>
2022-04-26 22:33:53 +0800

    rc: rc control works

--- src/service/software/motion_control/src/motion_control_service.c
+++ src/service/software/motion_control/src/motion_control_service.c
@@ -8,6 +8,9 @@
  */
 
 #include "motion_control/motion_control_service.h"
+
+#include <math.h>
+
 #include "speed_control/speed_control_service.h"
 
 K_THREAD_STACK_DEFINE(motion_service_stack, 1024);
@@ -44,23 +47,37 @@ _Noreturn void MotionServiceLoop(void *p1, void *p2, void *p3) {
   DesiredMotion desired_motion;
   DesiredRpm target_rpm;
 
+  const float r = 0.0188;
+  const float track = 0.185;
+  const float gear_ratio = 30;
+
   while (1) {
     int64_t t0 = k_loop_start();
 
     if (k_msgq_get(def->interface.desired_motion_msgq_in, &desired_motion,
                    K_NO_WAIT) == 0) {
       // calculate desired left/right speed from desired motion
+      float omega_l = 0, omega_r = 0;
+      omega_l = (desired_motion.linear - desired_motion.angular * track) / r;
+      omega_r = (desired_motion.linear + desired_motion.angular * track) / r;
 
-      printk("desired motion: %.4f, %.4f, desired rpm: %d, %d\n",
-             desired_motion.linear, desired_motion.angular,
-             target_rpm.motors[0], target_rpm.motors[1]);
+      target_rpm.motors[0] = omega_l * 60 / (2 * M_PI);
+      target_rpm.motors[1] = omega_r * 60 / (2 * M_PI);
 
-      //      while (k_msgq_put(
-      //                 def->dependencies.speed_control_interface->desired_rpm_msgq_in,
-      //                 &target_rpm, K_NO_WAIT) != 0) {
-      //        k_msgq_purge(
-      //            def->dependencies.speed_control_interface->desired_rpm_msgq_in);
-      //      }
+      target_rpm.motors[1] = -target_rpm.motors[1];
+
+      printk(
+          "desired motion: %.4f, %.4f, w_L: %.4f, w_R: %.4f, desired rpm: %d, "
+          "%d\n",
+          desired_motion.linear, desired_motion.angular, omega_l, omega_r,
+          target_rpm.motors[0], target_rpm.motors[1]);
+
+      while (k_msgq_put(
+                 def->dependencies.speed_control_interface->desired_rpm_msgq_in,
+                 &target_rpm, K_NO_WAIT) != 0) {
+        k_msgq_purge(
+            def->dependencies.speed_control_interface->desired_rpm_msgq_in);
+      }
     }
 
     k_msleep_until(def->tconf.period_ms, t0);
